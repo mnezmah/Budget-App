d88c62780e06013c1053b3277df0cb227af55fcf
mnezmah <mario.nezmah@gmail.com>
2019-09-18 16:45:18 +0200

    Create ExpenseList component

--- src/app.js
+++ src/app.js
@@ -20,10 +20,9 @@ const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses)
 
 const jsx = (
-
   <Provider store={store}>
     <AppRouter />
-    </Provider>
+  </Provider>
 )
 
 ReactDOM.render(jsx, document.getElementById('app'));
--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import ExpenseList from './ExpenseList'
 
 export default class ExpenseDashboardPage extends Component {
   render() {
     return (
       <div>
         Hello from Expense Dashboard page
+        <ExpenseList>
+
+        </ExpenseList>
       </div>
     )
   }

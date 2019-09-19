56336bba573c0a2a3922ee33e07be3361a70ab51
mnezmah <mario.nezmah@gmail.com>
2019-09-19 14:19:43 +0200

    Set up Add expense from

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react'
 
 export default class ExpenseForm extends Component {
+
+  state = {
+    description: '',
+    note: '',
+    amount: ''
+  }
+
+  onDescriptionChange = (e) => {
+    this.setState(
+      { description: e.target.value }
+    )
+  }
+
+  onNoteChange = (e) => {
+    this.setState({ note: e.target.value })
+  }
+
+  onAmountChange = (e) => {
+    const amount = e.target.value
+    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+      this.setState(() => ({ amount }))
+    }
+  }
+
   render() {
+    console.log('STATE:', this.state)
     return (
       <div>
-        ExpenseFrom
+        <form action="">
+          <input
+            value={this.state.description}
+            onChange={this.onDescriptionChange}
+            type="text"
+            placeholder="Description"
+            autoFocus />
+          <input
+            type="number"
+            value={this.state.amount}
+            onChange={this.onAmountChange}
+            placeholder="Amount" />
+          <textarea value={this.state.note}
+            onChange={this.onNoteChange} placeholder="Add a note for your expense"></textarea>
+          <button>Add Expensese</button>
+        </form>
       </div>
     )
   }

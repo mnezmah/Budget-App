6ba923feae3500065e1556f9d31dd36679d53f97
mnezmah <mario.nezmah@gmail.com>
2019-09-19 16:39:47 +0200

    Creted Link for editing an expense

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { removeExpense } from '../actions/expenses'
+import { Link } from 'react-router-dom'
 
 const ExpenseListItem = ({ iod, dispatch, description, amount, createdAt }) => {
   return (
-    <div>
+    <Link to={`/edit/${iod}`}>
       <p>{description} | {amount} | {createdAt}</p>
       <button onClick={() => {
         dispatch(removeExpense({iod}))
       }}>Remove</button>
-    </div>
+    </Link>
   )
 }
 

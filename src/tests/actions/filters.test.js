79fd324dab1aaf442456038f13493d772259d8bd
mnezmah <mario.nezmah@gmail.com>
2019-09-24 19:38:17 +0200

    Set up tests for actions/filters

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,57 @@
+import moment from 'moment'
+import {
+  setStartDate,
+  setEndDate,
+  sortByAmount,
+  setTextFilter,
+  sortByDate
+} from '../../actions/filters'
+
+test('should generate set start date action object', () => {
+  const action = setStartDate(moment(0))
+  expect(action).toEqual({
+    type: 'SET_START_DATE',
+    startDate: moment(0)
+  })
+})
+
+test('should generate set end date action object', () => {
+  const action = setEndDate(moment(0))
+  expect(action).toEqual({
+    type: 'SET_END_DATE',
+    endDate: moment(0)
+  })
+})
+
+test('should generate set text filter action object with given data', () => {
+  const text = 'something'
+  const action = setTextFilter(text)
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text: text
+  })
+})
+
+test('should generate set text filter action object with default data', () => {
+  const text = ''
+  const action = setTextFilter(text)
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text: text
+  })
+})
+
+
+test('should generate sort by date action object', () => {
+  const action = sortByDate()
+  expect(action).toEqual({
+    type: 'SORT_BY_DATE'
+  })
+})
+
+test('should generate sort by amount action object', () => {
+  const action = sortByAmount()
+  expect(action).toEqual({
+    type: 'SORT_BY_AMOUNT'
+  })
+})

704b8fc443c883c8470abf8f4091e175a8f0e20d
mnezmah <mario.nezmah@gmail.com>
2019-10-01 14:21:52 +0200

    Add test for rendering empty ExpenseList component

--- src/tests/components/expenseList.test.js
+++ src/tests/components/expenseList.test.js
@@ -5,5 +5,11 @@ import expenses from '../fixtures/expenses'
 
 test('should render ExpenseList with expenses', () => {
   const wrapper = shallow(<ExpenseList expenses={expenses} />)
-  expect(wrapper).toMatchSnapshot
+  expect(wrapper).toMatchSnapshot()
+})
+
+
+test('should render ExpenseList with no expenses message', () => {
+  const wrapper = shallow(<ExpenseList expenses={[]} />)
+  expect(wrapper).toMatchSnapshot()
 })

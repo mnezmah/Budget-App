49ec811880ad8f7fa055d49108362a17c4d23d7e
mnezmah <mario.nezmah@gmail.com>
2019-10-01 14:02:57 +0200

    Add if statement for rednering expenses

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,18 +1,24 @@
 import React from 'react'
-import { connect} from 'react-redux'
+import { connect } from 'react-redux'
 import ExpenseListItem from './ExpenseListItem'
 import selectExpenses from '../selectors/expenses'
 
-export const ExpenseList= (props) => {     
-    <div>
-   {props.expenses.map(expense => {
-     return <ExpenseListItem 
-      key={expense.id}
-     {...expense}
-     />
-   })}
-   </div>
-}
+export const ExpenseList = (props) => (
+  <div>
+    {
+      props.expenses.length === 0 ? (
+        <p>No expenses</p>
+      ) : (
+          props.expenses.map((expense) => {
+            return <ExpenseListItem
+              key={expense.id}
+              {...expense}
+            />
+          })
+        )
+    }
+  </div>
+)
 
 const mapStateToProps = (state) => {
   return {

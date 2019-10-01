2cfa4b1285b94b2835facfb394df5ad49b053798
mnezmah <mario.nezmah@gmail.com>
2019-10-01 19:38:29 +0200

    Refactor AddExpensePage to class component

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -5,16 +5,25 @@ import { addExpense } from '../actions/expenses'
 
 
 
-const AddExpensePage = (props) => (  
-  <div>
-    <h1> Add Expense</h1>
-    <ExpenseForm
-      onSubmit={(expense) => {
-        props.dispatch(addExpense(expense))
-        props.history.push('/')
-      }}
-    />
-  </div>
+export class AddExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    this.props.onSubmit(expense)
+    this.props.history.push('/')
+  }
+  render() {
+    return (
+      <div>
+        <h1> Add Expense</h1>
+        <ExpenseForm
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => (
+  { onSubmit: (expense) => dispatch(addExpense(expense)) }
 )
 
-export default connect()(AddExpensePage)
+export default connect(undefined, mapDispatchToProps)(AddExpensePage)
---
+++ src/tests/components/AddExpensePage.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import AddExpensePage from '../../components/AddExpensePage'
+import expenses from '../fixtures/expenses'
+
+test('should', ()=> {
+  const wrapper = shallow(<AddExpensePage />)
+})

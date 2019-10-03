4001887f98a17c8613b792d0ebfcd9a7aaf593db
mnezmah <mario.nezmah@gmail.com>
2019-10-03 15:17:19 +0200

    Create ExpenseListFlter test file

---
+++ src/tests/components/ExpenseListFilters.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import expenses from '../fixtures/expenses'
+import { ExpenseListFilter } from '../../components/ExpenseListFilters'
+
+test('should', () => {
+  
+})

00dcc1b88fb082f283ec8f6c209a22632c7e1f81
mnezmah <mario.nezmah@gmail.com>
2019-10-15 14:02:06 +0200

    Add Thunk to Redux store

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,8 +10,22 @@ const config = {
     appId: "1:213726614025:web:8a9b6679121f5ceb0eded8"
 }
 
+// const config = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.FIREBASE_DATABASE_URL,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+// };
+
 
 firebase.initializeApp(config)
 firebase.database().ref().set({
   name: 'Andrew Mead'
+})
+
+database.ref('notes').push({
+  title: 'Chumbva',
+  body: 'bjezi odavde'
 })
